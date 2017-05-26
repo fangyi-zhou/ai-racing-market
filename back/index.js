c555ff4f31ecdd5e7339cc87143c3dac6514fb60
Ruiao Hu <rh2015@ic.ac.uk>
2017-05-26 19:57:39 +0100

    back index.js periodic update time

--- back/index.js
+++ back/index.js
@@ -3,5 +3,20 @@ var p2  =  require('p2');
 var PORT = process.env.PORT || 1140;
 var io = require('socket.io').listen(PORT);
 
-var allGameObjects = new HashMap();
+var allRacers = new hashMap();
 
+console.log('Server Started. Listening......\n');
+
+// on add to client to observer when on connection.
+io.sockets.on('connection', function(connection){
+    //add emitting code here
+});
+
+var time = 0;
+setInterval(function(){
+   allRacers.forEach(function (value, key) {
+       //update information of each racer.
+   });
+   console.log('Server healthy...' + time);
+   time++;
+}, 2000);
