9479bf70637c48c5f24297c4496bd468486535e9
Ruiao Hu <rh2015@ic.ac.uk>
2017-05-26 17:50:10 +0100

    add to package.json

--- back/index.js
+++ back/index.js
@@ -1,11 +1,7 @@
-var HashMap = require('hashmap');
+var hashMap = require('hashmap');
 var p2  =  require('p2');
 var PORT = process.env.PORT || 1140;
 var io = require('socket.io').listen(PORT);
-
-var FRAME_RATE = 30;
-
-var BULLET_SPLASH = 800;
 
 var allGameObjects = new HashMap();
 
