83945417b63098ab90c5f02fe28df7a04ab6b5ea
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-05-31 00:44:52 +0100

    [Test][Host] test set steerValue/engineForce

--- test/host.test.js
+++ test/host.test.js
@@ -23,6 +23,9 @@ describe("Host", function() {
             childStub.getChildByCarId = (_) => {
                 return mockChild;
             };
+            raceBackStub.applyMove = (control, _) => {
+                output = control;
+            };
             host = proxyquire('../usercode/host', {
                 './child' : childStub,
                 '../environment/raceBack': raceBackStub
@@ -31,6 +34,14 @@ describe("Host", function() {
         it("Should get the speed of the car", function() {
             host.processUserOutput(0, "get speed");
             assert.equal(output, "0.5\n", "speed");
-        })
+        });
+        it("Should steer the car", function() {
+            host.processUserOutput(0, "set steerValue 0.1");
+            assert.equal(output.steerValue, 0.1, "steerValue");
+        });
+        it("Should set engine force of the car", function() {
+            host.processUserOutput(0, "set engineForce 0.1");
+            assert.equal(output.engineForce, 0.1, "engineForce");
+        });
     })
 });
