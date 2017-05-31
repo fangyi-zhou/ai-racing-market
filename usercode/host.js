4729ac7f521de86e517a5f46f2b37798312f9782
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-05-31 15:09:57 +0100

    [Host] processUserOutput is now able to handle multi-line commands

--- usercode/host.js
+++ usercode/host.js
@@ -8,47 +8,48 @@ function processUserOutput(carId, data) {
         console.error(`Attempting to read input from child ${carId}`);
         return;
     }
-    data = String(data);
-    console.log(`Child ${carId} Output: ${data}`);
-    let splatInput = data.split(" ");
-    let control = {};
-    if (splatInput.length === 0) return;
-    switch (splatInput[0]) {
-        case "set":
-            if (splatInput.length < 3) return;
-            switch (splatInput[1]) {
-                case "engineForce":
-                    const newEngineForce = parseFloat(splatInput[2]);
-                    control["engineForce"] = newEngineForce;
-                    console.log(`Set engineForce of ${carId} to ${newEngineForce}`);
-                    break;
-                case "steerValue":
-                    const newSteerValue = parseFloat(splatInput[2]);
-                    control["steerValue"] = newSteerValue;
-                    console.log(`Set steerValue of ${carId} to ${newSteerValue}`);
-                    break;
-                default:
-                    console.error(`Cannot set ${splatInput[1]} for Child ${carId}`)
-            }
-            raceBack.applyMove(control, child.carId);
-            break;
-        case "get":
-            if (splatInput.length < 2) return;
-            const car = child.car;
-            if (car === undefined || car === null) {
-                console.error(`Cannot find car with carId ${child.carId}`);
-            }
-            switch (splatInput[1]) {
-                case "speed":
-                    writeToUserInput(carId, car.getSpeed());
-                    break;
-                default:
-                    console.error(`Cannot get ${splatInput[1]} for Child ${carId}`)
-            }
-            break;
-        default:
-            console.error(`Cannot decode ${data} from Child ${carId}`);
-    }
+    const lines = String(data).split(/\r?\n/);
+    lines.forEach((data) => {
+        console.log(`Child ${carId} Output: ${data}`);
+        let splatInput = data.split(/\s+/);
+        let control = {};
+        if (splatInput.length < 2) return;
+        switch (splatInput[0]) {
+            case "set":
+                if (splatInput.length < 3) return;
+                switch (splatInput[1]) {
+                    case "engineForce":
+                        const newEngineForce = parseFloat(splatInput[2]);
+                        control["engineForce"] = newEngineForce;
+                        console.log(`Set engineForce of ${carId} to ${newEngineForce}`);
+                        break;
+                    case "steerValue":
+                        const newSteerValue = parseFloat(splatInput[2]);
+                        control["steerValue"] = newSteerValue;
+                        console.log(`Set steerValue of ${carId} to ${newSteerValue}`);
+                        break;
+                    default:
+                        console.error(`Cannot set ${splatInput[1]} for Child ${carId}`)
+                }
+                raceBack.applyMove(control, child.carId);
+                break;
+            case "get":
+                const car = child.car;
+                if (car === undefined || car === null) {
+                    console.error(`Cannot find car with carId ${child.carId}`);
+                }
+                switch (splatInput[1]) {
+                    case "speed":
+                        writeToUserInput(carId, car.getSpeed());
+                        break;
+                    default:
+                        console.error(`Cannot get ${splatInput[1]} for Child ${carId}`)
+                }
+                break;
+            default:
+                console.error(`Cannot decode ${data} from Child ${carId}`);
+        }
+    })
 }
 
 function writeToUserInput(carId, message) {
