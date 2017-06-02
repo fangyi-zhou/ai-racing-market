cc135605bf34d451f0acae426a75d3e55a7ba6a0
Ruiao Hu <rh2015@ic.ac.uk>
2017-06-02 15:30:45 +0100

    [src/app]dashboard skeleton

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,3 +2,15 @@
  * Created by ruiaohu on 02/06/2017.
  */
 import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector:'dashboard',
+  templateUrl:'./dashboard.component.html',
+  styleUrls:['./dashboard.component.css']
+})
+
+export class DashboardComponent implements OnInit{
+  ngOnInit():void{
+
+  }
+}
