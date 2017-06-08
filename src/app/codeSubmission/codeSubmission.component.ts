afaa3d1ade29e2693bdfa8a7505ea7747ec831ad
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-06-08 17:36:37 +0100

    [codeSubmission] implement submitScript

--- src/app/codeSubmission/codeSubmission.component.ts
+++ src/app/codeSubmission/codeSubmission.component.ts
@@ -1,23 +1,31 @@
 
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {CodeEditorService} from "../code-editor.service";
+import {ScriptService} from "../scripts/script.service";
+import {Script} from "../scripts/script";
 
 @Component({
   selector:'codeSubmission',
   templateUrl:'./codeSubmission.component.html',
   styleUrls:['./codeSubmission.component.css'],
-  providers:[CodeEditorService]
+  providers:[CodeEditorService, ScriptService]
 })
 
 export class CodeSubmissionComponent implements OnInit{
 
-  constructor(private codeEditorService: CodeEditorService) { }
+  constructor(private codeEditorService: CodeEditorService, private scriptService: ScriptService) { }
 
   ngOnInit():void{
     this.codeEditorService.loadCodeEditor();
   }
 
-    onSubmit() {
-        this.codeEditorService.foo();
-    }
+  submitScript() {
+      let script: Script = {
+          code : this.codeEditorService.getCode()
+      };
+      this.scriptService.createScript(script).then((newScript: Script) => {
+          // TODO : tell user upload complete
+          console.log("%o", newScript);
+      });
+  };
 }
