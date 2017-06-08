38b8171575a2ac8cff02274c29680fdfa1811aa8
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-06-08 14:39:10 +0100

    [CodeEditor] use ace CodeEditor

---
+++ src/app/code-editor.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+declare const ace : any;
+
+@Injectable()
+export class CodeEditorService {
+
+    loadCodeEditor() {
+        console.log("executed");
+        var editor = ace.edit("editor");
+        editor.setTheme("ace/theme/monokai");
+        editor.getSession().setMode("ace/mode/javascript");
+    }
+}
--- src/app/codeSubmission/codeSubmission.component.ts
+++ src/app/codeSubmission/codeSubmission.component.ts
@@ -1,14 +1,19 @@
 
 import { Component, OnInit } from '@angular/core';
+import {CodeEditorService} from "../code-editor.service";
 
 @Component({
   selector:'codeSubmission',
   templateUrl:'./codeSubmission.component.html',
-  styleUrls:['./codeSubmission.component.css']
+  styleUrls:['./codeSubmission.component.css'],
+  providers:[CodeEditorService]
 })
 
 export class CodeSubmissionComponent implements OnInit{
-  ngOnInit():void{
 
+  constructor(private codeEditorService: CodeEditorService) { }
+
+  ngOnInit():void{
+    this.codeEditorService.loadCodeEditor();
   }
 }
