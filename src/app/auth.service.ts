fe9d0399b6ae260376952572077edc209909118e
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-06-10 01:35:57 +0100

    [Auth] Implement register, refactor code

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -12,27 +12,46 @@ export class AuthService {
     }
 
     private authUrl: string = "/api/auth";
-    private SALT: string = 'c3d0a626a2d854fd3ec8be3b26bde137b49e28dbe50b92ac89c1442e321159845310b49b2156595a116270e2af2b9412';
+    private registerUrl: string = "/api/register";
+    static SALT: string = 'c3d0a626a2d854fd3ec8be3b26bde137b49e28dbe50b92ac89c1442e321159845310b49b2156595a116270e2af2b9412';
 
-    login(credential, successCallback, failureCallback) {
-        const hashedPass = asmCrypto.SHA256.hex(credential.password + this.SALT);
-        const credentialWithHashedPass = {
+    static hashPassword(credential) {
+        const hashedPass = asmCrypto.SHA256.hex(credential.password + AuthService.SALT);
+        return {
             username: credential.username,
             password: hashedPass
         };
+    }
+
+    static handleSuccess(data, successCallback) {
+        const token: string = data.id_token;
+        localStorage.setItem('id_token', token);
+        if (successCallback)
+            successCallback(token);
+    }
+
+    static handleFailure(error, failureCallback) {
+        if (failureCallback)
+            failureCallback(error)
+    }
+
+    login(credential, successCallback, failureCallback) {
+        const credentialWithHashedPass = AuthService.hashPassword(credential);
         this.http.post(this.authUrl, credentialWithHashedPass)
             .map(res => res.json())
             .subscribe(
-                data => {
-                    const token: string = data.id_token;
-                    localStorage.setItem('id_token', token);
-                    if (successCallback)
-                        successCallback(token);
-                },
-                error => {
-                    if (failureCallback)
-                        failureCallback(error)
-                }
+                data => AuthService.handleSuccess(data, successCallback),
+                error => AuthService.handleFailure(error, failureCallback)
+            );
+    }
+
+    register(credential, successCallback, failureCallback) {
+        const credentialWithHashedPass = AuthService.hashPassword(credential);
+        this.http.post(this.registerUrl, credentialWithHashedPass)
+            .map(res => res.json())
+            .subscribe(
+                data => AuthService.handleSuccess(data, successCallback),
+                error => AuthService.handleFailure(error, failureCallback)
             );
     }
 
