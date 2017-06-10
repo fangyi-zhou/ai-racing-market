816f48406e67c49d10ea02c0e0342b5ac83b7830
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-06-10 01:21:49 +0100

    [Auth] Hash password at front end

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,6 +3,8 @@ import {Http} from "@angular/http";
 import "rxjs/add/operator/map";
 import {tokenNotExpired} from "angular2-jwt";
 
+declare const asmCrypto: any;
+
 @Injectable()
 export class AuthService {
 
@@ -10,9 +12,15 @@ export class AuthService {
     }
 
     private authUrl: string = "/api/auth";
+    private SALT: string = 'c3d0a626a2d854fd3ec8be3b26bde137b49e28dbe50b92ac89c1442e321159845310b49b2156595a116270e2af2b9412';
 
     login(credential, successCallback, failureCallback) {
-        this.http.post(this.authUrl, credential)
+        const hashedPass = asmCrypto.SHA256.hex(credential.password + this.SALT);
+        const credentialWithHashedPass = {
+            username: credential.username,
+            password: hashedPass
+        };
+        this.http.post(this.authUrl, credentialWithHashedPass)
             .map(res => res.json())
             .subscribe(
                 data => {
