6cb8349ed4da309b60bf946fe883e39b820456c3
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-06-15 21:38:14 +0100

    [landing] Redirect to dashboard if logged in

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+      if (this.auth.loggedIn())
+      {
+          this.router.navigate(["./app-dashboard"]);
+      }
   }
 
 }
