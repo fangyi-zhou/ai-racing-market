6f7531403c6589a7cded94679476b0ec4845a1e5
Robert <Robert@dyn1181-192.wlan.ic.ac.uk>
2017-06-16 16:38:58 +0100

    [RH] Added new maps and began challenges.

---
+++ src/app/challenges/challenges.component.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from '@angular/core';
+import {ScriptService} from '../scripts/script.service';
+import { Script } from '../scripts/script';
+import { AuthService } from '../auth.service';
+
+declare var communication: any;
+
+@Component({
+    selector: 'app-training',
+    templateUrl: './challenges.component.html',
+    styleUrls: ['./challenges.component.css'],
+    providers: [ScriptService, AuthService]
+})
+export class TrainingComponent implements OnInit {
+    scripts: Script[];
+    selectedScript: Script;
+    constructor(private trainingService: ScriptService, private auth: AuthService) {}
+
+    ngOnInit(): void {
+        if (this.auth.loggedIn()) {
+            this.trainingService
+                .getUserScript(this.auth.userName())
+                .then((script: Script[]) => {
+                    this.scripts = script.map((script) => {
+                        // TODO some mapping for raw script json
+                        return script;
+                    });
+                    console.log(this.scripts);
+                });
+        }
+        communication.initGraphics();
+    }
+    onSelect(script: Script): void {
+        this.selectedScript = script;
+    }
+    trainAi(script: Script): void {
+        // TODO: clear graphics
+        communication.disconnectOnSwap();
+        communication.init(1337);
+        communication.train(script._id);
+    }
+    zoomIn(): void {
+        communication.zoomIn();
+    }
+    zoomOut(): void {
+        communication.zoomOut();
+    }
+    switchCar(): void {
+        communication.switchCar();
+    }
+}
+
